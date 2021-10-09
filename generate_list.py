80638ce8fd49807ed4d64b7e072f4fb40032a6ed
Kishan Tripathi <kshantripathi2011@gmail.com>
2021-10-09 14:32:03 +0530

    first commit

---
+++ generate_list.py
@@ -0,0 +1,162 @@
+
+
+import sys
+import os
+from random import randint    # Used for generating a seed.
+from hashlib import md5       # Used for hash iterations of the seed.
+
+''' GLOBALS '''
+
+IS_DEBUG = True
+
+OUT_CLIENT_PATH = "client"
+OUT_SERVER_PATH = "server"
+OUT_CLIENT_FILE = "client_passwords.txt"
+OUT_SERVER_FILE = "server_password.txt"
+
+MAX_SEED = 1024  # Entropy of passwords based on this value.
+
+# Program Arguments
+REQUIRED_NUM_ARGS = 2  # Require prog name and num iters
+MAXIMUM_NUM_ARGS = 2
+
+PROGRAM_ARG_NUM = 0    # i.e. sys.argv[0]
+NUM_ITERS_ARG_NUM = 1  # i.e. sys.argv[1]
+
+
+''' CLASSES '''
+
+
+class FileService:
+    # Class vars:
+    #   * cfile - Client file to contain list of passwords
+    #   * sfile - Server file to contain first password
+    def __init__(self):
+        print_debug("Creating FileService...")
+        if not os.path.exists(OUT_CLIENT_PATH):
+            os.makedirs(OUT_CLIENT_PATH)
+        if not os.path.exists(OUT_SERVER_PATH):
+            os.makedirs(OUT_SERVER_PATH)
+        cf = open(os.path.join(OUT_CLIENT_PATH, OUT_CLIENT_FILE), "w")
+        sf = open(os.path.join(OUT_SERVER_PATH, OUT_SERVER_FILE), "w")
+        self.cfile = cf
+        self.sfile = sf
+        print_debug("Successful init of FileService")
+
+    def write(self, list_passwords):
+        """Writes passwords to output files."""
+        try:
+            self.write_client_file(list_passwords)
+            self.write_server_file(list_passwords)
+        finally:
+            self.cfile.close()
+            self.sfile.close()
+
+    def write_client_file(self, list_passwords):
+        """Writes passwords to client output file."""
+        self.cfile.write("%s\n" % str(list_passwords[1:]))
+
+    def write_server_file(self, list_passwords):
+        """Writes first password to server output file."""
+        self.sfile.write("%s\n" % str(list_passwords[0]))
+
+
+''' FUNCTIONS '''
+
+
+def usage():
+    """Prints the usage/help message for this program."""
+    program_name = sys.argv[PROGRAM_ARG_NUM]
+    print("Usage:")
+    print("%s NUM_ITERS" % program_name)
+    print("  NUM_ITERS : Number of iterations to hash pass; positive integer.")
+
+
+def error_quit(msg, code):
+    """Prints out an error message, the program usage, and terminates with an
+       error code of `code`."""
+    print("[!] %s" % msg)
+    usage()
+    sys.exit(code)
+
+
+def validate_iters(num_iters):
+    """Verifies that the provided argument for number of hash iterations is
+       a valid int greater than 0."""
+    try:
+        num_iters = int(num_iters)
+        if num_iters < 1:
+            raise ValueError("Arg for num iterations must be a positive int!")
+    except ValueError:
+        error_quit("Arg for num iterations must be a positive int!", 400)
+    except Exception:
+        error_quit("Arg for num iterations must be a positive int!", 400)
+    return num_iters
+
+
+def parse_args():
+    """Gets and returns provided arguments."""
+    if len(sys.argv) < REQUIRED_NUM_ARGS or len(sys.argv) > MAXIMUM_NUM_ARGS:
+        error_quit("Incorrect number of arguments!", 400)
+    num_iters = sys.argv[NUM_ITERS_ARG_NUM]
+    num_iters = validate_iters(num_iters)
+    return num_iters
+
+
+def generate_passwords(num_iters):
+    """Returns list of OTPs based on the 8 middle characters of an MD5-hashed
+       pseudorandom number."""
+    list_passwords = []
+
+    # Get initial hash
+    seed = str(randint(0, MAX_SEED))  # Generate random number.
+    p = do_hash(seed)
+    list_passwords.append(p)
+
+    # Get n hashes based on initial hash
+    for i in range(0, num_iters):
+        p = do_hash(p)
+        list_passwords.append(p)
+    list_passwords.reverse()  # Start with the n-th hash iteration.
+    return list_passwords
+
+
+def do_hash(pword):
+    hashed = md5(pword.encode()).hexdigest()  # MD5 hash of that random number.
+    otp = hashed[12:20]  # Grab middle 8 chars of MD5 hash
+    return otp
+
+
+''' DEBUG '''
+
+
+def print_debug(msg):
+    """Prints if we are in debug mode."""
+    if IS_DEBUG:
+        print(msg)
+
+
+''' MAIN '''
+
+
+def main():
+    """Main driver that parses args & creates objects."""
+    print_debug("Starting...")
+    print_debug("Args: %s" % str(sys.argv))
+    num_iters = parse_args()
+    print_debug("Received argument: %s" % num_iters)
+    try:
+        file_service = FileService()
+    except:
+        error_quit("Failed to initialize FileService", 500)
+    list_passwords = generate_passwords(num_iters)
+    print_debug("Generated password list: %s" % str(list_passwords))
+    file_service.write(list_passwords)
+    print_debug("Done!")
+
+
+''' PROCESS '''
+
+
+if __name__ == '__main__':
+    main()
---
+++ server/server.py
@@ -0,0 +1,188 @@
+
+import socket
+import sys
+import threading
+from hashlib import md5       # Used for hash iterations of the otp.
+from datetime import datetime # Used to timestamp logs.
+
+''' GLOBALS '''
+
+IS_DEBUG = True
+
+OUT_SERVER_FILE = "server_password.txt"
+
+PROGRAM_ARG_NUM = 0    # i.e. sys.argv[0]
+PORT_ARG_NUM = 1       # i.e. sys.argv[1], optional port arg
+
+DEFAULT_PORT = 40000
+BUFF_SIZE = 1024
+
+MSG_SERVER_SUCCESS = "Authentication Successful!\n"
+MSG_SERVER_FAILURE = "Authentication Failure!\n"
+
+''' CLASSES '''
+
+
+class ClientConnectedThread(threading.Thread):
+    """New thread for every connected client. Each thread
+       handles an individual client's requests."""
+    # Class vars:
+    #   * client_ip - String
+    #   * client_port - String
+    #   * client_sock - Socket
+    #   * lock - Mutex lock
+    #   * file_service - Service for reading/writing to password file.
+    def __init__(self, ip, port, sock, lock):
+        """Initialize args to class variables."""
+        threading.Thread.__init__(self)
+        print_debug(datetime.now())
+        self.client_ip = ip
+        self.client_port = port
+        self.client_sock = sock
+        self.lock = lock
+        self.file_service = FileService()
+
+    def run(self):
+        """Driver for individual thread. Handles client requests."""
+        print_debug("Host %s:%s has connected!" % (self.client_ip, self.client_port))
+        while True:
+            try:
+                # Get otp from client
+                otp_recv = self.parse_client_request().decode('utf-8').rstrip('\r\n')
+                print_debug("User sent over: %s" % otp_recv)
+                self.lock.acquire()
+                try:
+                    serv_msg = self.authenticate_otp(otp_recv)
+                    self.client_sock.send(serv_msg.encode())
+                finally:
+                    self.client_sock.close()
+                    self.lock.release()
+                    return
+            except Exception as e:
+                print_debug("Encountered error: " + str(e))
+
+    def parse_client_request(self):
+        try:
+            msg_rec = self.client_sock.recv(BUFF_SIZE)
+        except socket.error:
+            print_debug("Connection error, unable to receive command.", 400)
+        except:
+            print_debug("An unknown error occurred, unable to receive command.", 400)
+        return msg_rec
+
+    def authenticate_otp(self, otp_recv):
+        last_password = self.file_service.get_password_from_file()
+        print_debug("Server's last stored password: %s" % last_password)
+        hashed_user_password = self.do_hash(otp_recv)
+        print_debug("Hashed user password is: %s" % hashed_user_password)
+        if hashed_user_password == last_password:
+            return self.successful_authentication(self.file_service, otp_recv)
+        else:
+            return self.failed_authentication()
+
+    def do_hash(self, pword):
+        hashed = md5(pword.encode()).hexdigest()  # MD5 hash of that random number.
+        otp = hashed[12:20]  # Grab middle 8 chars of MD5 hash
+        return otp
+
+    def successful_authentication(self, file_service, new_password):
+        print(MSG_SERVER_SUCCESS)
+        file_service.update_password_in_file(new_password)
+        return MSG_SERVER_SUCCESS
+
+    def failed_authentication(self):
+        print(MSG_SERVER_FAILURE)
+        return MSG_SERVER_FAILURE
+
+
+class FileService:
+    def __init__(self):
+        print_debug("Creating FileService...")
+        print_debug("Successful init of FileService")
+
+    def get_password_from_file(self):
+        """Parses string literal from server password file."""
+        try:
+            sfile = open(OUT_SERVER_FILE, "r")
+            last_pword = sfile.readline().rstrip('\n')
+        except Exception:
+            error_quit("Failed to read from file %s" % sfile.name, 500)
+        return last_pword
+
+    def update_password_in_file(self, new_password):
+        """Updates password in server password file with last-used password."""
+        try:
+            sfile = open(OUT_SERVER_FILE, "w")
+            sfile.write("%s\n" % new_password)
+        finally:
+            sfile.close()
+
+
+''' FUNCTIONS '''
+
+
+def usage():
+    """Prints the usage/help message for this program."""
+    program_name = sys.argv[PROGRAM_ARG_NUM]
+    print("\nUsage:")
+    print("\t%s" % program_name)
+    print("\tFile \"%s\" must exist and be populated with the next password it expects." % OUT_SERVER_FILE)
+
+
+def error_quit(msg, code):
+    """Prints out an error message, the program usage, and terminates with an
+       error code of `code`."""
+    print("\n[!] %s" % msg)
+    usage()
+    sys.exit(code)
+
+
+def handle_client_connections(server_port):
+    """Driver that creates a socket and handles connections."""
+    print_debug("Starting server.")
+    try:
+        lock = threading.Lock()
+        # Create socket, connect to host and port.
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('', server_port))
+        s.listen(1)
+        print_debug("Server running.")
+        while 1:
+            try:
+                conn, addr = s.accept()
+                ip = addr[0]
+                port = addr[1]
+                client = ClientConnectedThread(ip, port, conn, lock)  # New thread.
+                client.start()
+            except Exception as e:
+                print("Encountered error: " + str(e))
+    except socket.error as e:
+        print("Unable to connect due to " + str(e))
+
+
+''' DEBUG '''
+
+
+def print_debug(msg):
+    """Prints if we are in debug mode."""
+    if IS_DEBUG:
+        print(msg)
+
+
+''' MAIN '''
+
+
+def main():
+    """Main driver."""
+    print_debug("Starting...")
+    port = sys.argv[PORT_ARG_NUM] if len(sys.argv) == PORT_ARG_NUM + 1 else DEFAULT_PORT
+    port = int(port)
+    handle_client_connections(port)
+    print_debug("Done!")
+
+
+''' PROCESS '''
+
+
+if __name__ == '__main__':
+    main()
